2c8ff9b1b8c29c4e9129c336d29604a418bb3029
Liang Huang <sammy.huang@zilliz.com>
2024-03-06 20:05:52 +0800

    .
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>

--- examples/data-sources/zilliz_cloud_regions/data-source.tf
+++ examples/data-sources/zilliz_cloud_regions/data-source.tf
@@ -11,10 +11,27 @@ provider "zilliz" {
   cloud_region_id = "gcp-us-west1"
 }
 
-data "zilliz_cloud_regions" "example" {
+data "zilliz_cloud_regions" "region01" {
+  cloud_id = "aws"
+}
+
+data "zilliz_cloud_regions" "region02" {
   cloud_id = "gcp"
 }
 
-output "ouput" {
-  value = data.zilliz_cloud_regions.example.cloud_regions
+data "zilliz_cloud_regions" "region03" {
+  cloud_id = "azure"
+}
+
+output "aws_ouput" {
+  value = data.zilliz_cloud_regions.region01.cloud_regions
+}
+
+
+output "gcp_ouput" {
+  value = data.zilliz_cloud_regions.region02.cloud_regions
+}
+
+output "azure_ouput" {
+  value = data.zilliz_cloud_regions.region03.cloud_regions
 }
