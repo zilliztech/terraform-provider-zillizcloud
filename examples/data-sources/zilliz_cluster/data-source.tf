1c739ab5c65985b66694ab17755e9baf3dd14fdb
Liang Huang <sammy.huang@zilliz.com>
2024-03-06 20:51:35 +0800

    chore
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>
    
    .
    
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>
    
    .
    
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>
    
    .
    
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>

--- examples/data-sources/zilliz_cluster/data-source.tf
+++ examples/data-sources/zilliz_cluster/data-source.tf
@@ -11,18 +11,12 @@ provider "zilliz" {
   cloud_region_id = "gcp-us-west1"
 }
 
-data "zilliz_projects" "test" {
-}
-
-resource "zilliz_cluster" "test" {
-  plan         = "Standard"
-  cluster_name = "Cluster-Test1"
-  cu_size      = 1
-  cu_type      = "Performance-optimized"
-  project_id   = data.zilliz_projects.test.projects[0].project_id
-}
 
 data "zilliz_cluster" "test" {
   id = zilliz_cluster.test.id
+  id = "in03-bd4013ae76e3a72"
 }
 
+output "output" {
+  value = data.zilliz_cluster.test
+}
