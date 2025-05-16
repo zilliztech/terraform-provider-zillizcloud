2d039442c41cd4d59b708ea722393417c6886f85
Kiyor <cai@kiyor.com>
2025-05-16 04:16:49 +0000

    add missing file and doc

---
+++ client/partitions.go
@@ -0,0 +1,61 @@
+package client
+
+type CreatePartitionsParams struct {
+	DbName         string `json:"dbName"`
+	PartitionsName string `json:"aliasName"`
+	CollectionName string `json:"collectionName"`
+}
+
+func (c *ClientCollection) CreatePartitions(params *CreateCollectionParams) error {
+	params.DbName = c.dbName
+	var resp zillizResponse[any]
+	err := c.do("POST", "v2/vectordb/partitions/create", params, &resp)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type DropPartitionsParams struct {
+	DbName         string `json:"dbName"`
+	PartitionsName string `json:"aliasName"`
+}
+
+func (c *ClientCollection) DropPartitions(params *DropPartitionsParams) error {
+	params.DbName = c.dbName
+	var resp zillizResponse[any]
+	err := c.do("POST", "v2/vectordb/partitions/drop", params, &resp)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type ListPartitionsesParams struct {
+	DbName string `json:"dbName"`
+}
+
+func (c *ClientCollection) ListPartitionses(params *ListPartitionsesParams) ([]string, error) {
+	params.DbName = c.dbName
+	var resp zillizResponse[[]string]
+	err := c.do("POST", "v2/vectordb/partitions/list", params, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Data, nil
+}
+
+type DescribePartitionsParams struct {
+	DbName         string `json:"dbName"`
+	PartitionsName string `json:"aliasName"`
+}
+
+func (c *ClientCollection) DescribePartitions(params *DescribePartitionsParams) (any, error) {
+	params.DbName = c.dbName
+	var resp zillizResponse[any]
+	err := c.do("POST", "v2/vectordb/partitions/describe", params, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Data, nil
+}
